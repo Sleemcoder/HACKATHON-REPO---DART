727412f66851c2405b7e913cd629042d4ee57b9e
Sleemcoder <delight0994@gmail.com>
2024-08-31 14:05:46 +0100

    fixed errors

--- Basic_Calc.dart
+++ Basic_Calc.dart
@@ -1,43 +1,44 @@
-void main () {
-double basicCalculator(double num1, double num2, String operator) {
-  switch (operator) {
-    case '+':
-      return num1 + num2;
-    case '-':
-      return num1 - num2;
-    case '*':
-      return num1 * num2;
-    case '/':
-      if (num2 == 0) {
-        throw ArgumentError("Division by zero is not allowed.");
-      }
-      return num1 / num2;
-    case '%':
-      if (num2 == 0) {
-        throw ArgumentError("Division by zero is not allowed.");
-      }
-      return num1 % num2;
-    default:
-      throw ArgumentError("Invalid operator. Use +, -, *, /, or %.");
-  }
-}
-
-double extendedCalculator(String expression) {
-  List<String> parts = expression.split(' ');
-  if (parts.length != 3) {
-    throw ArgumentError("Invalid expression format. Use 'num1 operator num2'.");
+void main() {
+  double basicCalculator(double num1, double num2, String operator) {
+    switch (operator) {
+      case '+':
+        return num1 + num2;
+      case '-':
+        return num1 - num2;
+      case '*':
+        return num1 * num2;
+      case '/':
+        if (num2 == 0) {
+          throw ArgumentError("Division by zero is not allowed.");
+        }
+        return num1 / num2;
+      case '%':
+        if (num2 == 0) {
+          throw ArgumentError("Division by zero is not allowed.");
+        }
+        return num1 % num2;
+      default:
+        throw ArgumentError("Invalid operator. Use +, -, *, /, or %.");
+    }
   }
 
-  double num1 = double.parse(parts[0]);
-  String operator = parts[1];
-  double num2 = double.parse(parts[2]);
+  double extendedCalculator(String expression) {
+    List<String> parts = expression.split(' ');
+    if (parts.length != 3) {
+      throw ArgumentError(
+          "Invalid expression format. Use 'num1 operator num2'.");
+    }
 
-  return basicCalculator(num1, num2, operator);
-}
+    double num1 = double.parse(parts[0]);
+    String operator = parts[1];
+    double num2 = double.parse(parts[2]);
 
-print(basicCalculator(10, 5, '+'));
-  print(basicCalculator(10, 5, '-')); 
-  print(basicCalculator(10, 5, '*')); 
+    return basicCalculator(num1, num2, operator);
+  }
+
+  print(basicCalculator(10, 5, '+'));
+  print(basicCalculator(10, 5, '-'));
+  print(basicCalculator(10, 5, '*'));
   print(basicCalculator(10, 5, '/'));
   print(basicCalculator(10, 5, '%'));
 
