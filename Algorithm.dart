b902befc5aba0e4b803d17a609259724542dc3c4
Sleemcoder <delight0994@gmail.com>
2024-08-31 13:45:58 +0100

    implement bubble sort

--- Algorithm.dart
+++ Algorithm.dart
@@ -32,4 +32,21 @@ void main () {
   List<int> sortedList = quickSort(unsortedList);
   print(sortedList); // Output: [10, 15, 23, 33, 76, 99]
 
+
+// Bubble Sort implementation
+  List<int> bubbleSort(List<int> list) {
+  int n = list.length;
+  for (int i = 0; i < n - 1; i++) {
+    for (int j = 0; j < n - i - 1; j++) {
+      if (list[j] > list[j + 1]) {
+        // Swap if the element is greater than the next one
+        int temp = list[j];
+        list[j] = list[j + 1];
+        list[j + 1] = temp;
+      }
+    }
+  }
+  return list;
+}
+
 }
