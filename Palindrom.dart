b23fe62a08f693ef5d4152b37291668a6ff4da0e
Sleemcoder <delight0994@gmail.com>
2024-08-31 13:02:34 +0100

    initialized pointers

--- Palindrom.dart
+++ Palindrom.dart
@@ -1,32 +1,31 @@
-
-
 void main() {
   bool isPalindrome(String s) {
-  // Remove spaces, punctuation, and convert to lowercase
-  String sanitizedInput = s.replaceAll(RegExp(r'[\W_]+'), '').toLowerCase();
+    // Remove spaces, punctuation, and convert to lowercase
+    String sanitizedInput = s.replaceAll(RegExp(r'[\W_]+'), '').toLowerCase();
 
-  // Initialize two pointers
-  int left = 0;
-  int right = sanitizedInput.length - 1;
+    // Initialize two pointers
+    int left = 0;
+    int right = sanitizedInput.length - 1;
 
-  // Compare characters from both ends
-  while (left < right) {
-    if (sanitizedInput[left] != sanitizedInput[right]) {
-      return false;
+    // Compare characters from both ends
+    while (left < right) {
+      if (sanitizedInput[left] != sanitizedInput[right]) {
+        return false;
+      }
+      left++;
+      right--;
     }
-    left++;
-    right--;
+
+    return true;
   }
 
-  return true;
-}
+  bool isNumericPalindrome(int number) {
+    // Convert the number to a string and check if it's a palindrome
+    return isPalindrome(number.toString());
+  }
 
-bool isNumericPalindrome(int number) {
-  // Convert the number to a string and check if it's a palindrome
-  return isPalindrome(number.toString());
-}
   print(isPalindrome("Was it a car or a cat I saw?")); // true
-  print(isPalindrome("hello"));   // false
+  print(isPalindrome("hello")); // false
   print(isPalindrome("Racecar")); // true
 
   print(isNumericPalindrome(12321)); // true
