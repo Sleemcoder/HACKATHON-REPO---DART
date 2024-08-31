cd4582dbced21cd2bfec0750426cc9f5a16bba45
Sleemcoder <delight0994@gmail.com>
2024-08-31 13:01:26 +0100

    Created strings

---
+++ Palindrom.dart
@@ -0,0 +1,34 @@
+
+
+void main() {
+  bool isPalindrome(String s) {
+  // Remove spaces, punctuation, and convert to lowercase
+  String sanitizedInput = s.replaceAll(RegExp(r'[\W_]+'), '').toLowerCase();
+
+  // Initialize two pointers
+  int left = 0;
+  int right = sanitizedInput.length - 1;
+
+  // Compare characters from both ends
+  while (left < right) {
+    if (sanitizedInput[left] != sanitizedInput[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+
+  return true;
+}
+
+bool isNumericPalindrome(int number) {
+  // Convert the number to a string and check if it's a palindrome
+  return isPalindrome(number.toString());
+}
+  print(isPalindrome("Was it a car or a cat I saw?")); // true
+  print(isPalindrome("hello"));   // false
+  print(isPalindrome("Racecar")); // true
+
+  print(isNumericPalindrome(12321)); // true
+  print(isNumericPalindrome(12345)); // false
+}
